779175ee1225131080ef933c7cef5e1f54ebcbc9
Luis Fernando Machado <>
2023-08-31 00:37:23 -0300

    TDD, scope to automated software tests

---
+++ src/test/java/tests/entities/AccountTest.java
@@ -0,0 +1,36 @@
+package tests.entities;
+
+import com.student.luisf.entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
+
+public class AccountTest {
+    @Test
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+        //Arrange
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account acc = AccountFactory.createEmptyAccount();
+
+        //Act
+        acc.deposit(amount);
+
+        //Assert
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmmount() {
+        //Arrange
+        double amount = -200.0;
+        double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+
+        //Act
+        acc.deposit(amount);
+
+        //assert
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+}
